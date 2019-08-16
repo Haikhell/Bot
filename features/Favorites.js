4bb1c1a10a8e62af66dd70b920fae780fac5323b
Haikhell <ivanhaikhell@gmail.com>
2019-08-16 08:41:57 +0300

    Fix

--- features/Favorites.js
+++ features/Favorites.js
@@ -1,20 +1,15 @@
 const getFavorites = require('../helpers/db_function/favorite_get');
-const Eror = require('../../Bot/Errors');
 module.exports = function(controller) {
   controller.hears([ 'Favorites' ], 'message', async (bot, message) => {
-    try {
-      const tempObj = await getFavorites.fGet(message.user, 0);
-      await bot.reply(message, {
-        attachment: {
-          type: 'template',
-          payload: {
-            template_type: 'generic',
-            elements: tempObj
-          }
+    const tempObj = await getFavorites.fGet(message.user, 0);
+    await bot.reply(message, {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'generic',
+          elements: tempObj
         }
-      });
-    } catch (error) {
-      Eror();
-    }
+      }
+    });
   });
 };

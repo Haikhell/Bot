c1f49b71c6ad310785516cdd04763100ae141d9c
Haikhell <ivanhaikhell@gmail.com>
2019-08-16 14:17:06 +0300

    Error Favorites

--- Errors.js
+++ Errors.js
@@ -5,5 +5,7 @@ module.exports = async function(error) {
 
     case 305:
       return 'You have not made purchases your list is empty';
+    case 310:
+      return 'You have not made favorite your list is empty';
   }
 };
--- features/Favorites.js
+++ features/Favorites.js
@@ -1,15 +1,20 @@
 const getFavorites = require('../helpers/db_function/favorite_get');
+const Eror = require('../Errors');
 module.exports = function(controller) {
   controller.hears('Favorites', 'message', async (bot, message) => {
     const tempObj = await getFavorites.fGet(message.user, 0);
-    await bot.reply(message, {
-      attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'generic',
-          elements: tempObj
+    if (tempObj === null) {
+      await bot.reply(message, Eror(310));
+    } else {
+      await bot.reply(message, {
+        attachment: {
+          type: 'template',
+          payload: {
+            template_type: 'generic',
+            elements: tempObj
+          }
         }
-      }
-    });
+      });
+    }
   });
 };
